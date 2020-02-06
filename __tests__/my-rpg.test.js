8849c86e09ab87c0586136a8fcc670174279b046
Brandan Sayarath and Joshua Hellman <joshua@>
2020-02-06 14:32:45 -0800

    added 2 more tests and js to get tests to pass

--- __tests__/my-rpg.test.js
+++ __tests__/my-rpg.test.js
@@ -1,11 +1,21 @@
-import { Game } from "./../src/game"
+import { Game, generateNumber } from "./../src/game"
+
 
 describe('Game', () => {
-  let game = new Game
+  let game = new Game("mountain","Santa Cruz",0,0)
 
   test('should generate a random number', () => {
-    let randomNumber = game.rideBike();
-    expect(Number.isInteger(randomNumber)).toEqual(true);
+    let randomNumberUnderTen = generateNumber();
+    expect(randomNumberUnderTen).toBeLessThanOrEqual(10);
   })
+  test('should return "you fell off your bike" if checkRide() is passed a 1 as an argument', () => {
+    let StringOutput = game.checkRide(1);
+    expect(StringOutput).toEqual("you fell off your bike");
+  })
+  test('should return a number if checkRide() is passed a number greater then 1', () => {
+    let number = game.checkRide(2);
+    expect(Number.isInteger(number)).toEqual(true);
+  })
+  
 
 })
--- src/game.js
+++ src/game.js
@@ -5,18 +5,18 @@ export class Game {
   this.rexp = rexp;
   this.texp = texp;
   }
-  rideBike(){
-    let rexp = generateNumber()
-    // if (rexp === 1 ){
-    //   return("you fell off your bike")
-    // } else {
-    //   rexp += this.rexp
-    return rexp;
-    // } 
+  checkRide(number){
+    if (number === 1 ){
+      return("you fell off your bike")
+    } else {
+      this.rexp += number
+      console.log(this.rexp)
+      return this.rexp
+    } 
   }
-
 }
-function generateNumber(){
-  var number = Math.floor( Math.random() * 6 ) +1;
+
+export function generateNumber(){
+  var number = Math.floor( Math.random() * 10) +1;
   return number;
 }
